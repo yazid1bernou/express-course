6c90827006e1172870325cd44088fb5eee2d666a
unknown <yazid1bernou@gmail.com>
2023-12-06 09:48:38 +0000

    create function to add employee

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express =  require('express');
 const app = express();
+app.use(express('json'));
 const port = process.env.port || 5000
-
+const employees = [
+    { IDemp : 1 ,  fullName : "bernou adem" , Departement :  "Finance" } ,
+    { IDemp : 2 ,  fullName : "bernou yazid" , Departement :  "IT" } ,
+    { IDemp : 3 ,  fullName : "adami khaoula" , Departement :  "DRH" } ,
+]
 app.get('/' , (req , res ) =>  {
     res.send('hello my friend , I am BERNOU Adem');
 });
 
-app.get('/api/employees' , (req , res)=> {
-    res.send(['adem , ayoub , yazid , khaoula'])
+app.get('/api/employees' , (req , res) => {
+     res.send(employees);
 });
 app.get('/api/employees/:id' , (req , res)=> {
     res.send(req.params.id)
@@ -16,6 +21,16 @@ app.get('/api/employees/:name/:age' , (req , res)=> {
     res.send(req.params)
 });
 
+app.post('/api/employees' , (req , res) => {
+
+    const employee = {
+        IDemp : req.body.IDemp ,
+        fullName : req.body.fullName ,
+        Departement : req.body.Departement
+    }
+    employees.push(employee);
+    res.send(employee);
+});
 
 app.listen(port  , ()=> {
     console.log(" running in "+port)
